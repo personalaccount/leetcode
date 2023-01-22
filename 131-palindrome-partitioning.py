ed9b9835597ff9e9890e4c10d388f1cdcf74e67f
Philip <personalaccount@users.noreply.github.com>
2023-01-22 22:27:00 +0100

    solution explanation

--- 131-palindrome-partitioning.py
+++ 131-palindrome-partitioning.py
@@ -18,6 +18,18 @@ Constraints:
 s contains only lowercase English letters.
 '''
 
+'''
+Solution expalained:
+
+Using a backtracking approach. 
+Start with an empty partition, and for each substring of the given string "s" check if the substring is a palindrome. 
+
+If it is, add the substring to the partition and recursively call the backtracking function with the next substring. 
+If the entire string has been processed, add the partition to the result list. 
+
+Finally, return the result list, containing all possible partitions of the given string "s" into palindromes.
+'''
+
 def partition(s):
     def backtrack(start, partition, result):
         if start == len(s):
