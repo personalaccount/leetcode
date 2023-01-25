c01c7c68bdb75e0862a517166abb74fbff18b3d2
Philip <personalaccount@users.noreply.github.com>
2023-01-25 21:40:08 +0100

    added solution description

--- 2-add-two-numbers.py
+++ 2-add-two-numbers.py
@@ -10,6 +10,18 @@ Example:
 Input: l1 = [2,4,3], l2 = [5,6,4]
 Output: [7,0,8]
 Explanation: 342 + 465 = 807.
+
+Solution explained:
+
+This code takes in two linked lists, l1 and l2, representing two non-negative integers. 
+The digits in each linked list are stored in reverse order, and each node contains a single digit. 
+The function addTwoNumbers() iterates through the linked lists.
+For each corresponding pair of digits it adds them together and the carry from the previous addition. 
+It then updates the carry for the next iteration. 
+If one of the linked lists is longer than the other, the function continues to add the remaining digits from the longer list with 0. 
+Finally, if there is a carry left over, it creates a new node to store it. The function returns the resulting linked list.
+
+The above example of l1=[2,4,3] and l2=[5,6,4] will return [7,0,8], which is the sum of 342 and 465.
 '''
 
 # Definition for singly-linked list.
